83bd4e9a3ced37318cd145f932a1b62cae75d968
Elliott Thompson <ellth92@gmail.com>
2015-06-21 19:31:47 +0100

    updates

--- site/navigation.js
+++ site/navigation.js
@@ -1,4 +1,4 @@
-function createList(name, listItems) {
+function createList(listItems) {
 	var ul = document.createElement("ul");
 	for (var item of listItems) {
 		var li = document.createElement("li");
@@ -6,15 +6,26 @@ function createList(name, listItems) {
 		ul.innerHTML += li.outerHTML;
 	}
 	document.body.innerHTML += ul.outerHTML;
-	return ul;
-};
+}
 function jsonNavToObject(jsonString) {
-};
+
+}
+function getTopNavItems(jsonString) {
+	var items = [];
+	var json = JSON.parse(jsonString);
+	for (var item of json.NavArray) {
+		items.push(item.Navigation);
+	}
+	return items;
+}
+function generateNavigation() {
+	var json = readJsonFromURL('nav.json');
+	var topNavigationData = getTopNavItems(json);
+	createList(topNavigationData);
+}
+
 function readJsonFromURL(url) {
-	$.getJSON(url, function(url){
-		console.log(url);
-	});
-	document.body.innerHTML += $.getJSON(url);
-};
+	return "{\"NavArray\":[{\"Navigation\":\"Cars\",\"URL\":\"/cars/\",\"SubNavigation\":[{\"Name\":\"Mazda3\", \"URL\":\"/cars/mazda3/\"},{\"Name\":\"Mazda2\", \"URL\":\"/cars/mazda2/\"},{\"Name\":\"Mazda5\", \"URL\":\"/cars/mazda5/\"}]},{\"Navigation\":\"Dealers\", \"URL\":\"/dealers/\",\"SubNavigation\":[{\"Name\":\"Essex\", \"URL\":\"/dealers/essex/\"},{\"Name\":\"Kent\", \"URL\":\"/dealers/kent/\"},{\"Name\":\"London\", \"URL\":\"/dealers/london/\"}]},{\"Navigation\":\"Products\", \"URL\":\"/products/\",\"SubNavigation\":[{\"Name\":\"Sugar\", \"URL\":\"/products/sugar/\"},{\"Name\":\"Cocao\", \"URL\":\"/products/cocao/\"},{\"Name\":\"Rice\", \"URL\":\"/products/rice/\"}]}]}";
+}
 function switchSubNavigation(newSubNav) {
-};
+}
